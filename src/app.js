13f2c2b61da0232e30da987328db898c198ce40d
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-06-04 22:05:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,40 +1,50 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  document.querySelector("#button").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = excuseGenerator();
-  });
-};
-
-function excuseGenerator() {
-  let who = ["A genie", "A sorcerer", "Zeus", "My cat"];
-  let action = ["magicked away", "zapped", "crushed", "exploded"];
-  let what = ["my spellbook", "my dragon", "my broom", "my dignity"];
-  let when = [
-    "before the class.",
-    "when I was on stage.",
-    "while I was exercising.",
-    "during my morning meditation.",
-    "while I was exploring Mars."
+function generateNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
   ];
+  let numRandomizer = Math.floor(Math.random() * numbers.length);
 
-  let subject = Math.floor(Math.random() * who.length);
-  let verb = Math.floor(Math.random() * action.length);
-  let object = Math.floor(Math.random() * what.length);
-  let tempClause = Math.floor(Math.random() * when.length);
-
-  return (
-    who[subject] +
-    " " +
-    action[verb] +
-    " " +
-    what[object] +
-    " " +
-    when[tempClause]
-  );
+  return numbers[numRandomizer];
 }
+
+function generateSuit() {
+  let suit = ["♥", "♦", "♠", "♣"];
+  let suitRandomizer = Math.floor(Math.random() * suit.length);
+
+  return suit[suitRandomizer];
+}
+
+function generateCard() {
+  let suit = generateSuit();
+  let number = generateNumber();
+
+  if (suit === "♥" || suit === "♦") {
+    document.querySelector(".topIcon").classList.add("redSuit");
+    document.querySelector(".bottomIcon").classList.add("redSuit");
+  } else {
+    document.querySelector(".topIcon").classList.remove("redSuit");
+    document.querySelector(".bottomIcon").classList.remove("redSuit");
+  }
+
+  document.querySelector(".topIcon").innerHTML = suit;
+  document.querySelector(".bottomIcon").innerHTML = suit;
+  document.querySelector(".number").innerHTML = number;
+}
+
+window.onload = generateCard();
+document.querySelector("button").addEventListener("click", generateCard);
+setInterval(generateCard, 10000);
